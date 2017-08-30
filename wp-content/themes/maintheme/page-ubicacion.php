5de135056ed882747563de27968ea99a6bbead3f
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2017-08-30 18:35:56 -0500

    Moddified map call

--- wp-content/themes/maintheme/page-ubicacion.php
+++ wp-content/themes/maintheme/page-ubicacion.php
@@ -12,7 +12,7 @@
       <p class="phone"><?php the_field('telefono'); ?></p>
     </div>
 
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d940.3738929862725!2d-99.14213037083921!3d19.477303463537137!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f900e0219d0d%3A0x938ff9ded88c6723!2sPte.+112+157%2C+Panamericana%2C+07770+Ciudad+de+M%C3%A9xico%2C+CDMX!5e0!3m2!1ses-419!2smx!4v1501363920768" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235.0934657942668!2d-99.14163879685339!3d19.477308598895014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f900e0470001%3A0xd9267fe3b338b33e!2sMi+mascota!5e0!3m2!1ses!2smx!4v1504125676205" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
 
   </div>
 <?php endwhile ?>
